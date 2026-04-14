3733a91d33e854f12d6388ad736888cce31f118d
Vivek Thapar <vivekthapar3@gmail.com>
2026-04-14 19:44:20 +0530

    Tailwind installation and testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import VisualizerCanvas from "./components/Canvas"
 function App() {
   return (
     <div className="w-screen h-screen bg-black">
+      <div className="bg-fuchsia-600 text-white p-4 flex items-center h-20 justify-center">
+          Tailwind Test
+      </div>
       <VisualizerCanvas />
     </div>
   )
