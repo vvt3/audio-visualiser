75d1b27fac1c59a1dcfa702f42af73aa1c91676d
Vivek Thapar <vivekthapar3@gmail.com>
2026-04-16 01:39:03 +0530

    Added smooth bars

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,6 +5,10 @@ export default function Canvas({ audioFile, volume }) {
   const canvasRef = useRef(null)
   const analyserRef = useRef(null)
   const audioRef = useRef(null)
+  const smoothDataRef = useRef(null)
+
+
+  const BAR_SCALE = 0.85
 
   useEffect(() => {
     if (!audioFile) return
@@ -32,8 +36,18 @@ export default function Canvas({ audioFile, volume }) {
     const ctx = canvas.getContext("2d")
 
     function resize() {
-      canvas.width = window.innerWidth
-      canvas.height = window.innerHeight
+      const dpr = window.devicePixelRatio || 1
+
+      const width = window.innerWidth
+      const height = window.innerHeight
+
+      canvas.width = width * dpr
+      canvas.height = height * dpr
+
+      canvas.style.width = width + "px"
+      canvas.style.height = height + "px"
+
+      ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
     }
 
     resize()
@@ -46,22 +60,48 @@ export default function Canvas({ audioFile, volume }) {
         return
       }
       const data = analyser.getFrequencyData()
+      if (!smoothDataRef.current) {
+        smoothDataRef.current = new Float32Array(data.length)
+      }
+
+      //------------ Drawing BLOCK
+
+      const width = canvas.clientWidth
+      const heightCanvas = canvas.clientHeight
 
       // clear bg
       ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, canvas.width, canvas.height)
+      ctx.fillRect(0, 0, width, heightCanvas)
 
       // bars
       ctx.fillStyle = "white"
-      const gap = 1
-      const barWidth = (canvas.width - data.length * gap) / data.length
 
+      const gap = 1
+      const barWidth = (width - data.length * gap) / data.length
+
+
+       if (!smoothDataRef.current) {
+        smoothDataRef.current = new Float32Array(data.length)
+      }
+
+      const smoothData = smoothDataRef.current
+      
       data.forEach((value, i) => {
         const x = i * (barWidth + gap)
-        const height = value
+        //const height = value * BAR_SCALE
+        //const height = (value / 255) * heightCanvas * BAR_SCALE
+        // smoothing (lerp)
+        smoothData[i] = smoothData[i] * 0.8 + value * 0.2
+        const avg = (data[i] + data[i-1] + data[i+1]) / 3
+        //const normalized = value / 255
+        const normalized = smoothData[i] / 255
+        const height = Math.pow(normalized, 0.5) * heightCanvas * BAR_SCALE
+        const y = heightCanvas - height
 
-        ctx.fillRect(x, canvas.height - height, barWidth, height)
+        ctx.fillRect(x, y, barWidth, height)
       })
+
+      //------------ END Draw BLOCK
 
       animationId = requestAnimationFrame(loop)
     }
